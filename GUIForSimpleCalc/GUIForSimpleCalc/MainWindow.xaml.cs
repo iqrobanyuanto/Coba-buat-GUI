c6a6ce2c998c6d1fa2d49749386664068db41d72
Gapherion <iqrobanyuanto12345@gmail.com>
2023-03-09 09:28:51 +0700

    update calculasi

--- GUIForSimpleCalc/GUIForSimpleCalc/MainWindow.xaml.cs
+++ GUIForSimpleCalc/GUIForSimpleCalc/MainWindow.xaml.cs
@@ -24,5 +24,64 @@ namespace GUIForSimpleCalc
         {
             InitializeComponent();
         }
+        string var;
+        int max = 0;
+
+        private void but_1_Click(object sender, RoutedEventArgs e)
+        {
+            var = var + "1";
+            Output.Content = var;
+            max++;
+        }
+
+        private void but_2_Click(object sender, RoutedEventArgs e)
+        {
+            var = var + "2";
+            Output.Content = var;
+            max++;
+        }
+
+        private void but_3_Click(object sender, RoutedEventArgs e)
+        {
+            var = var + "3";
+            Output.Content = var;
+            max++;
+        }
+
+        private void but_4_Click(object sender, RoutedEventArgs e)
+        {
+            var = var + "4";
+            Output.Content = var;
+            max++;
+        }
+
+        private void but_plus_Click(object sender, RoutedEventArgs e)
+        {
+            var = var + "+";
+            Output.Content = var;
+            max++;
+        }
+
+        private void but_equal_Click(object sender, RoutedEventArgs e)
+        {
+            int num1;
+            int num2 = -00001;
+            int calc;
+            string op = "!";
+            int i = 0;
+            num1 = Convert.ToInt32(var.Substring(0,1));
+            num2 = Convert.ToInt32(var.Substring(2, 1));
+            Output.Content = num1 + num2;
+            /*
+            while (i < max)
+            {
+                if (num2 == -00001 || op == "!")
+                {
+        
+                i++;
+            }
+            */
+        }
+
     }
 }
